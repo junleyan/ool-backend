0fb3c63bd755f0dcbb279007853b6b28cc569061
Junle Yan <junleyan.hi@gmail.com>
2024-10-17 14:59:34 -1000

    Add separators to sidebar

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { ScrollArea } from "./ui/scroll-area";
 import TagMultiSelectCombobox from "./TagMultiSelectCombobox";
 import GroupMultiSelectCombobox from "./GroupMultiSelectCombobox";
 import { State } from "./App";
+import { Separator } from "./ui/separator";
 
 interface SidebarProps {
     state: State;
@@ -14,7 +15,7 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ state, dispatch }) => {
     return (
         <div className="flex h-full flex-col items-center justify-start p-6">
-            <div className="flex mb-5">
+            <div className="flex">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Seal_of_the_State_of_Hawaii.svg"
                     alt="Seal of the State of Hawaii"
@@ -26,9 +27,12 @@ const Sidebar: React.FC<SidebarProps> = ({ state, dispatch }) => {
                     <p className="text-sm text-gray-700"><b>open</b> and <b>transparent</b></p>
                 </div>
             </div>
+            <Separator className="mt-5 mb-3" />
             <ScrollArea>
                 <OrganizationCombobox state={state} dispatch={dispatch} />
+                <Separator className="my-3" />
                 <GroupMultiSelectCombobox state={state} dispatch={dispatch} />
+                <Separator className="my-3" />
                 <TagMultiSelectCombobox state={state} dispatch={dispatch} />
             </ScrollArea>
         </div>
