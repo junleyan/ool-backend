da8047eb3312aa0937c76114ac157e2afa3bc166
Justin <clashergamer808@gmail.com>
2024-10-24 22:35:08 -1000

    Add multi-colored format badges

--- frontend/components/DatasetsView.tsx
+++ frontend/components/DatasetsView.tsx
@@ -9,12 +9,38 @@ interface DatasetsViewProps {
     dispatch: Dispatch<{ type: string; payload: unknown }>;
 }
 
+interface resourceProps {
+    format: string;
+    name: string;
+    created: string;
+    state: string;
+    url: string;
+    position: number;
+}
+
 interface DatasetProps {
+    author: string;
+    license_id: string;
+    license_title: string;
     state: string;
     title: string;
     notes: string;
+    resources: resourceProps[];
     tags: Tag[];
 }
+
+const badgeColors = [
+    "#E53935", "#FF8A65", "#5D3FD3", "#00796B", "#6D4C41",
+    "#6BC04A", "#43A047", "#00ACC1", "#FFB300", "#039BE5", "#1E88E5",
+    "#3949AB", "#7E57C2", "#AB47BC", "#BC306A", "#FF5252", "#303030"
+];
+
+const formats = [
+    "CSV", "HTML", "ArcGIS GeoServices REST API", "GeoJSON", "ZIP", "KML", "JSON", "RDF", 
+    "XML", "OGC WFS", "OGC WMS", "XLSX", "KMZ", "XLS", "PDF", "MP4"
+];
+
+const formatColorMap = new Map(formats.map((format, index) => [format, badgeColors[index % badgeColors.length]]));
 
 const datasetArr = (sortBy: string, datasets: DatasetProps[], searchQuery: string) => {
     return datasets
@@ -47,11 +73,15 @@ const datasetArr = (sortBy: string, datasets: DatasetProps[], searchQuery: strin
                 notes={dataset.notes}
                 tags={dataset.tags}
                 state={dataset.state}
+                author={dataset.author}
+                license_id={dataset.license_id}
+                license_title={dataset.license_title}
+                resources={dataset.resources}
             />
         ));
 };
 
-const Dataset: React.FC<DatasetProps> = ({ title, notes, tags }) => {
+const Dataset: React.FC<DatasetProps> = ({ title, notes, tags, resources, author }) => {
     return (
         <Card className="w-full mb-4 shadow-md transition-transform transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-50">
             <CardHeader>
@@ -60,6 +90,22 @@ const Dataset: React.FC<DatasetProps> = ({ title, notes, tags }) => {
                     <div dangerouslySetInnerHTML={{ __html: notes }} />
                 </CardDescription>
             </CardHeader>
+            <CardFooter className="flex flex-wrap">
+                {resources
+                    .filter(resource => resource.state === 'active')
+                    .filter(resource => formats.includes(resource.format)) // !!! Change later ???
+                    .map((resource, index) => (
+                        <a key={index} href={resource.url} target="_blank">
+                            <Badge 
+                                className="m-0.5" 
+                                style={{ backgroundColor: formatColorMap.get(resource.format) }}
+                            >
+                                {resource.format}
+                            </Badge>
+                        </a>
+
+                ))}
+            </CardFooter>
             <CardFooter className="flex flex-wrap">
                 {tags.map((tag, index) => (
                     <Badge className="m-0.5" key={index}>{tag.display_name}</Badge>
