9e66e7e0a923df6b4f5432c010c97104dbeaf680
Junle Yan <junleyan.hi@gmail.com>
2024-11-03 02:35:17 -1000

    Add title and subtitle to XY axis API

--- backend/src/controllers/chatGptRoutes.js
+++ backend/src/controllers/chatGptRoutes.js
@@ -3,9 +3,9 @@ import { sendJsonResponse } from "../utils/jsonResponse.js";
 import { getXYAxis } from "../services/chatGptApi.js";
 
 export const handleGetXYAxis = async (req, res) => {
-    const { csv } = req.query;
+    const { csv, info } = req.query;
     try {
-        sendJsonResponse(res, HttpStatusCode.Ok, '', await getXYAxis(csv));
+        sendJsonResponse(res, HttpStatusCode.Ok, '', await getXYAxis(csv, info));
     } catch (error) {
         sendJsonResponse(res, HttpStatusCode.BadRequest, error.message);
     }
