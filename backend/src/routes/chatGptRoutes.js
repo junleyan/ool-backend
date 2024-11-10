1db0e6aa95d40709df97e344ce7907fcb9ecaf6d
Junle Yan <junleyan.hi@gmail.com>
2024-11-10 04:07:30 -1000

    Add AI suggestion and persona API

--- backend/src/routes/chatGptRoutes.js
+++ backend/src/routes/chatGptRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { handleGetChatResponse, handleGetQuestionSuggestions, handleGetXYAxis } from '../controllers/chatGptRoutes.js';
+import { handleGetChatResponse, handleGetDatasetSuggestions, handleGetPersona, handleGetQuestionSuggestions, handleGetXYAxis } from '../controllers/chatGptRoutes.js';
 
 const chatGptRouter = Router();
 chatGptRouter.get('/get/xy', handleGetXYAxis);
 chatGptRouter.get('/get/question_suggestions', handleGetQuestionSuggestions);
 chatGptRouter.get('/get/chat', handleGetChatResponse);
+chatGptRouter.get('/get/dataset_suggestions', handleGetDatasetSuggestions);
+chatGptRouter.get('/get/persona', handleGetPersona);
 
 export default chatGptRouter;
